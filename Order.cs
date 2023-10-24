7bab3847e1c126fd693821b1500f43f22f04ebac
trygvebechsgaard <trygvemb@gmail.com>
2023-10-24 12:16:55 +0700

    First commit

---
+++ Order.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BonusApp
+{
+    public class Order
+    {
+        public BonusProvider Bonus { get; set; }
+
+
+        private List<Product> _products = new List<Product>();
+
+        public void AddProduct(Product p)
+        {
+            _products.Add(p);
+        }
+
+        public double GetValueOfProducts()
+        {
+            double productsSum = _products.Sum(p => p.Value);
+            return productsSum;
+        }
+
+        //// using Where()
+        //public double GetValueOfProducts(DateTime date)
+        //{
+        //    double productsSum = _products.Where(p => date >= p.AvailableFrom && date <= p.AvailableTo)
+        //                                  .Sum(p => p.Value);
+
+        //    return productsSum;
+        //}
+
+        public double GetValueOfProducts(DateTime date)
+        {
+            double productsSum = _products.Sum(p => date >= p.AvailableFrom && date <= p.AvailableTo ? p.Value : 0);
+
+            return productsSum;
+        }
+
+        public List<Product> SortProductOrderBy(Func<Product, object> keySelector)
+        {
+            List<Product> sortedProducts = _products.OrderBy(keySelector).ToList();
+
+            return sortedProducts;
+        }
+
+        public List<Product> SortProductOrderByAvailableTo()
+        {
+            List<Product> sortedProducts = _products.OrderBy(p => p.AvailableTo).ToList();
+
+            return sortedProducts;
+        }
+
+        public double GetBonus()
+        {
+            return Bonus(GetValueOfProducts());
+        }
+
+        public double GetBonus(Func<double, double> bonus)
+        {
+            return bonus(GetValueOfProducts());
+        }
+        public double GetBonus(DateTime date, Func<double, double> bonus)
+        {
+            return bonus(GetValueOfProducts(date));
+        }
+
+        //Func<int, int> compareTo = other => CompareTo(other)
+
+        public double GetTotalPrice()
+        {
+            return GetValueOfProducts() - GetBonus();
+        }
+        public double GetTotalPrice(Func<double, double> amount)
+        {
+            return GetValueOfProducts() - GetBonus(amount);
+        }
+        public double GetTotalPrice(DateTime date, Func<double, double> amount)
+        {
+            return GetValueOfProducts(date) - GetBonus(date, amount);
+        }
+
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿namespace BonusApp
+{
+    
+
+    internal class Program
+    {
+        delegate int AddNumbers(int x, int y);
+        
+        static void Main(string[] args)
+        {
+
+
+
+            AddNumbers Add = delegate (int x, int y)
+            {
+                int result = x + y;
+                return result;
+            };
+
+            Func<int, int, int> addWithLambda = (x, y) => x + y;
+            Func<int, int> addTenTimes = x => x * 10;
+            Func<int, int> addWithItself = x => x * x;
+
+            int sum = Add(5, 3);
+            int sum2 = addWithLambda(5, 3);
+            int sum3 = addTenTimes(10);
+            int sum4 = addWithItself(10);
+
+            Console.WriteLine("sum " + sum); // Output: 8
+            Console.WriteLine("sum2 " + sum2); // Output: 8
+            Console.WriteLine("sum3 " + sum3); // Output: 100
+            Console.WriteLine("sum4 " + sum4); // Output: 100
+
+
+            Console.ReadKey();
+
+            Console.WriteLine("Hi there. Run the unittests! Remember to debug.");
+
+
+        }
+        
+    }
+}
